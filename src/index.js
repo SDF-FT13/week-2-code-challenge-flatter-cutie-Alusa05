46b0a13e247b68c80b9234e2140fa9156ae916e5
Lisa Alusa <sayilisaalusa@gmail.com>
2025-03-24 11:51:25 +0300

    second part

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 // Your code here
 document.addEventListener('DOMContentLoaded', () => {
 const characterBar = document.getElementById('character-bar');
+const detailedInfo = document.getElementById('detailed-info');
+const votesForm = document.gerElementById('votes-form');
+const resetVotesButton = document.getElementById('reset-votes');
+const characterForm = document.getElementById('character-form');
+
+let characters = [];
+let currentCharacter = null;
+
+//Fetching the characters from the server
+fetch('http://localhost:3000/characters')
 });
