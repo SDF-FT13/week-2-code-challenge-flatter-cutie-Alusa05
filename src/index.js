865e350ad239b250fcaccff420d45486f10601c3
Lisa Alusa <sayilisaalusa@gmail.com>
2025-03-24 12:04:07 +0300

    part 3 of 1

--- src/index.js
+++ src/index.js
@@ -11,4 +11,20 @@ let currentCharacter = null;
 
 //Fetching the characters from the server
 fetch('http://localhost:3000/characters')
+.then(response => response.json())
+.then(data => {
+    characters = data;
+    displayCharacters(data);
+});
+
+//Displaying the characters
+function displayCharacters(characters) {
+    characterBar.innerHTML = '';
+    characters.forEach(character => {
+        const span = document.createElement('span');
+        span.textContent = character.name;
+        span.addEventListener('click', () => showCharacterDetails(character));
+        characterBar.appendChild(span);
+    });
+}
 });
